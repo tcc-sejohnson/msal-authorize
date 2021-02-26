a7797c75845589b94605fde54c42ce85e4acb587
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2021-02-25 18:52:10 -0700

    Initial draft of transition is done

--- src/components/routes/HiddenRoute.tsx
+++ src/components/routes/HiddenRoute.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Route, RouteProps } from 'react-router-dom';
-import HiddenComponent, { HiddenComponentProps } from '../../private_components/HiddenComponent';
+import HiddenComponent, { HiddenComponentProps } from '../private_components/HiddenComponent';
 
 /**
  * Renders its children if the user is in one of the allowed roles,
  * else renders the unauthorizedRedirect ReactNode.
  */
 const HiddenRoute: React.VFC<HiddenComponentProps & RouteProps> = ({
+  userRoles,
   allowedRoles,
   allBut,
+  isAuthenticating,
   authenticatingComponent,
   children,
   ...rest
@@ -17,7 +19,13 @@ const HiddenRoute: React.VFC<HiddenComponentProps & RouteProps> = ({
     <Route
       {...rest}
       render={() => (
-        <HiddenComponent allowedRoles={allowedRoles} allBut={allBut} authenticatingComponent={authenticatingComponent}>
+        <HiddenComponent
+          userRoles={userRoles}
+          allowedRoles={allowedRoles}
+          allBut={allBut}
+          isAuthenticating={isAuthenticating}
+          authenticatingComponent={authenticatingComponent}
+        >
           {children}
         </HiddenComponent>
       )}
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,14 @@
-import { ProvideAuth, useAuth, authorize, DefaultRole, LoginProvider } from './auth/auth';
-import AuthBrowserRouter from './components/router/routers/AuthBrowserRouter';
-import AuthHashRouter from './components/router/routers/AuthHashRouter';
-import AuthMemoryRouter from './components/router/routers/AuthMemoryRouter';
-import AuthStaticRouter from './components/router/routers/AuthStaticRouter';
-import { useAuthRoutes } from './components/router/routers/AuthRouter';
-import HiddenRoute from './components/router/routes/HiddenRoute';
-import LoginOrUnauthorizedRedirectRoute from './components/router/routes/LoginOrUnauthorizedRedirectRoute';
-import LoginRedirectRoute from './components/router/routes/LoginRedirectRoute';
-import ProtectedRoute from './components/router/routes/ProtectedRoute';
-import UnauthorizedRedirectRoute from './components/router/routes/UnauthorizedRedirectRoute';
+import { authorize } from './auth/auth';
+import HiddenRoute from './components/routes/HiddenRoute';
+import ProtectedRoute from './components/routes/ProtectedRoute';
+import UnauthorizedRedirectRoute from './components/routes/UnauthorizedRedirectRoute';
 import HiddenComponent from './components/private_components/HiddenComponent';
 import ProtectedComponent from './components/private_components/ProtectedComponent';
 
-export type { AuthorizationContext, Roles, User, ProvideAuthProps } from './auth/auth';
+export type { Roles } from './auth/auth';
 export type { HiddenComponentProps } from './components/private_components/HiddenComponent';
 export type { ProtectedComponentProps as UnauthorizedComponentProps } from './components/private_components/ProtectedComponent';
-export type { LoginOrUnauthorizedRedirectRouteProps } from './components/router/routes/LoginOrUnauthorizedRedirectRoute';
-export type { LoginRedirectRouteProps } from './components/router/routes/LoginRedirectRoute';
-export type { ProtectedRouteProps } from './components/router/routes/ProtectedRoute';
-export type { UnauthorizedRedirectRouteProps } from './components/router/routes/UnauthorizedRedirectRoute';
+export type { ProtectedRouteProps } from './components/routes/ProtectedRoute';
+export type { UnauthorizedRedirectRouteProps } from './components/routes/UnauthorizedRedirectRoute';
 
-export {
-  DefaultRole,
-  LoginProvider,
-  ProvideAuth,
-  useAuth,
-  authorize,
-  HiddenComponent,
-  ProtectedComponent,
-  useAuthRoutes,
-  AuthBrowserRouter,
-  AuthHashRouter,
-  AuthMemoryRouter,
-  AuthStaticRouter,
-  HiddenRoute,
-  LoginOrUnauthorizedRedirectRoute,
-  LoginRedirectRoute,
-  ProtectedRoute,
-  UnauthorizedRedirectRoute,
-};
+export { authorize, HiddenComponent, ProtectedComponent, HiddenRoute, ProtectedRoute, UnauthorizedRedirectRoute };
